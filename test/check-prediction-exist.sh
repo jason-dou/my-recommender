2fc91a5957e18c33c17afd48d494794b0f215651
jason-dou <jasondou25@gmail.com>
2023-02-09 20:52:20 -0500

    Update prediction result check to match main

--- test/check-prediction-exist.sh
+++ test/check-prediction-exist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PREDICTION_RESULT="src/dataset/movies.pred.1tsv"
+PREDICTION_RESULT="src/dataset/movies.pred.tsv"
 
 if [[ ! -f $PREDICTION_RESULT ]] ; then
     echo "File ${PREDICTION_RESULT} is not there, aborting."
