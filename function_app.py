45ab4e8b98bf0fc17eae58e1377e48e0098d1d47
jason-dou <jasondou25@gmail.com>
2023-02-14 23:58:33 -0500

    Add role authorization

--- function_app.py
+++ function_app.py
@@ -1,24 +1,37 @@
 import azure.functions as func
 import logging
+
 from data_handler import get_recommendation
+from auth import AuthError, verify_roles, handle_auth_error
 
 app = func.FunctionApp()
 
-# Learn more at aka.ms/pythonprogrammingmodel
-
-# Get started by running the following code to create a function using a HTTP trigger.
 
 @app.function_name(name="MoviesRecommender")
 @app.route(route="movies", auth_level=func.AuthLevel.ANONYMOUS)
 def get_movies_recommendation(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        logging.info('Getting Movies Recommendation Request.')
+        # verify_roles(req, "Recommendation.Read")
+        movies_result = get_recommendation()
 
-    movies_result = get_recommendation()
-    print(movies_result)
-    
-    return func.HttpResponse(
-        movies_result,
-        status_code=200,
-        mimetype='application/json'
-    )
-        
+        return func.HttpResponse(
+            movies_result,
+            status_code=200,
+            mimetype='application/json'
+        )
+    except AuthError as ae:
+        logging.error(ae)
+        error_response = handle_auth_error(ae)
+
+        return func.HttpResponse(
+            error_response,
+            status_code=401,
+            mimetype='application/json'
+        )
+    except Exception as e:
+        logging.error(e)
+        return func.HttpResponse(
+            "Internal Error",
+            status_code=500,
+        )
